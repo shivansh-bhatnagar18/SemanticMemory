2d0242e06b5f63832e4a9a342d0158058b93efa1
shivansh-bhatnagar18 <shivansh111sid@gmail.com>
2023-11-07 09:55:12 +0530

    Removed Bugs

--- continuous_skip_gram/continuous_skip_gram_model.py
+++ continuous_skip_gram/continuous_skip_gram_model.py
@@ -2,7 +2,6 @@ import pandas as pd
 import nltk
 import string
 import matplotlib.pyplot as plt
-
 from nltk.corpus import stopwords
 from nltk import word_tokenize
 from gensim.models import Word2Vec as w2v
@@ -17,31 +16,12 @@ with open(PATH, 'r') as f:
     for l in f:
         lines.append(l)
 
-# remove new lines
 lines = [line.rstrip('\n') for line in lines]
-
-# make all characters lower
 lines = [line.lower() for line in lines]
-
-# remove punctuations from each line
 lines = [line.translate(str.maketrans('', '', string.punctuation)) for line in lines]
-
-# tokenize
 lines = [word_tokenize(line) for line in lines]
 
 def remove_stopwords(lines, sw = sw):
-    '''
-    The purpose of this function is to remove stopwords from a given array of 
-    lines.
-    
-    params:
-        lines (Array / List) : The list of lines you want to remove the stopwords from
-        sw (Set) : The set of stopwords you want to remove
-        
-    example:
-        lines = remove_stopwords(lines = lines, sw = sw)
-    '''
-    
     res = []
     for line in lines:
         original = line
